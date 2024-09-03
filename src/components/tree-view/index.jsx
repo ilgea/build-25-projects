c2117c984820163897c48893123af281159095e4
ilgea <zubeyirozkaya@gmail.com>
2024-09-03 17:22:31 +0300

    updated index.jsx

--- src/components/tree-view/index.jsx
+++ src/components/tree-view/index.jsx
@@ -1,9 +1,12 @@
 import MenuList from "./menu-list";
 import "./styles.css";
 
+// menus probu sağlanmazsa default olarak boş bir array gönderilecek
 export default function TreeView({ menus = [] }) {
   return (
     <div className="tree-view-container">
+      {/* // MenuList componentine menus propu gönderiliyor.  */}
+      {/* // MenuList componenti içerisinde list propu ile menus propu alınıyor. */}
       <MenuList list={menus} />
     </div>
   );
