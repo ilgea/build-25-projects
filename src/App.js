6cd416e0ef943ddc4418800a1582ecc2a5c27371
ilgea <zubeyirozkaya@gmail.com>
2024-09-03 13:29:42 +0300

    added load-more-data to App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import "./App.css";
-import Accordion from "./components/accordions";
-import ImageSlider from "./components/image-slider";
+import TreeView from "./components/tree-view";
+import menus from "./components/tree-view/data";
+// import Accordion from "./components/accordions";
+// import RandomColor from "./components/random-color";
+// import StarRating from "./components/star-raiting";
+// import ImageSlider from "./components/image-slider";
 // import Slayder from "./components/image-slider/Slayder";
-import RandomColor from "./components/random-color";
-import StarRating from "./components/star-raiting";
+// import LoadMoreData from "./components/load-more-data";
 
 function App() {
   return (
@@ -15,13 +18,12 @@ function App() {
       {/* ---------- Star Rating Component ---------- */}
       {/* <StarRating noOfStars={10} /> */}
       {/* ---------- Image Slider Components ---------- */}
-      <ImageSlider
-        url={"https://picsum.photos/v2/list"}
-        page={"1"}
-        limit={10}
-      />
+      {/* <ImageSlider url={"https://picsum.photos/v2/list"} page={"1"} limit={10}/> */}
       {/* <Slayder url={"https://picsum.photos/v2/list"} page={"1"} limit={10} /> */}
-      {/*  */}
+      {/* ----------- Load More Products Component ----------- */}
+      {/* <LoadMoreData /> */}
+      {/* ----------------- Tree View component/menu UI component / recursive navigation menu --------------- */}
+      <TreeView menus={menus} />
     </div>
   );
 }
