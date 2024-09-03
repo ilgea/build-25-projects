f8bd56321b015498b3d4e9561f5b2371070bb8c2
ilgea <zubeyirozkaya@gmail.com>
2024-09-03 14:04:38 +0300

    added TreeView component

---
+++ src/components/tree-view/menu-item.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import MenuList from "./menu-list";
+import { FaMinus, FaPlus } from "react-icons/fa";
+
+export default function MenuItem({ item }) {
+  // if the item has children, then render the children
+  // otherwise, render the item
+  const [displayCurrentChildren, setDisplayCurrentChildren] = useState({});
+  const handleToggleChildren = (getCurrentLabel) => {
+    setDisplayCurrentChildren({
+      ...displayCurrentChildren,
+      [getCurrentLabel]: !displayCurrentChildren[getCurrentLabel],
+    });
+  };
+
+  console.log(displayCurrentChildren);
+
+  return (
+    <li>
+      <div className="menu-item">
+        <p>{item.label}</p>
+        {item && item.children && item.children.length ? (
+          <span onClick={() => handleToggleChildren(item.label)}>
+            {displayCurrentChildren[item.label] ? (
+              <FaMinus color="#fff" size={12} />
+            ) : (
+              <FaPlus color="#fff" size={12} />
+            )}
+          </span>
+        ) : null}
+      </div>
+      {item &&
+      item.children &&
+      item.children.length > 0 &&
+      displayCurrentChildren[item.label] ? (
+        <MenuList list={item.children} />
+      ) : null}
+    </li>
+  );
+}
